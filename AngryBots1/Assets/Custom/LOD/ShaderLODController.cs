b611409852f902d3ecd8b0c4f56940d8af1a109a
Thorbjørn Wolf <thorbjorn@helga.in>
2014-03-28 12:10:33 +0100

    Shader LOD change implemented with distance between mouse and object as determining factor

--- AngryBots1/Assets/Custom/LOD/ShaderLODController.cs
+++ AngryBots1/Assets/Custom/LOD/ShaderLODController.cs
@@ -5,9 +5,38 @@ public class ShaderLODController : MonoBehaviour {
 
 	private Shader shader;
 
+	public int tempThresholdPixelCount = 200;
+
 	// Use this for initialization
 	void Start () {
 		shader = renderer.material.shader;
+	}
+
+	private float GetDistanceToFocus()
+	{
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 focusPos = GetFocusPosition();
+
+		return (focusPos - screenPos).magnitude;
+	}
+
+	private Vector3 GetFocusPosition()
+	{
+		// Use mouse position for now
+		return Input.mousePosition;
+	}
+
+	private int DetermineWantedLOD()
+	{
+		float distanceToFocus = GetDistanceToFocus();
+		if(distanceToFocus < tempThresholdPixelCount)
+		{
+			return 200;
+		}
+		else
+		{
+			return 50;
+		}
 	}
 	
 	// Update is called once per frame
@@ -17,7 +46,9 @@ public class ShaderLODController : MonoBehaviour {
 		// and function determining at what distance shader 
 		// change should occur
 
-		
+		shader.maximumLOD = DetermineWantedLOD();
+		return;
+
 		if(Input.GetKeyDown("z"))
 		{
 			shader.maximumLOD = 200;
