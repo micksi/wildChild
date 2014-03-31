5800365eb719f404ada684813a95dc4c9afd68a3
Thorbjørn Wolf <thorbjorn@helga.in>
2014-03-31 10:39:08 +0200

    Added Bootcamp project.

---
+++ BootCamp/Assets/Custom/FocusProvider.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class FocusProvider {
+
+	public static bool useMouse = true;
+
+	// Distance
+	public static float GetFocusDistance(Vector3 fromWorldPosition)
+	{
+		return GetFocusObjectDifference(fromWorldPosition).magnitude;
+	}
+
+	public static float GetNormalizedFocusDistance(Vector3 fromWorldPosition)
+	{
+		return GetNormalizedFocusObjectDifference(fromWorldPosition).magnitude;
+	}
+
+
+	// Object - focus difference vectors
+	private static Vector2 GetFocusObjectDifference(Vector3 worldPosition)
+	{
+		return GetFocusPosition() - GetObjectScreenPosition(worldPosition);
+	}
+
+	private static Vector2 GetNormalizedFocusObjectDifference(Vector3 worldPosition)
+	{
+		Vector2 pixelVector = GetFocusObjectDifference(worldPosition);
+		return NormalizeScreenPosition(pixelVector);
+	}
+
+
+	// Object screen position
+	private static Vector2 GetObjectScreenPosition(Vector3 worldPosition)
+	{
+		Camera cam = Camera.main;
+		Vector3 screenPos = cam.WorldToScreenPoint(worldPosition);
+		return new Vector2(screenPos.x, screenPos.y);
+	}
+
+	// Transform a position from coordinates in pixels to normalized
+	// screen coordinates.
+	private static Vector2 NormalizeScreenPosition(Vector2 pixelPosition)
+	{
+		Camera cam = Camera.main;
+		pixelPosition.x /= cam.pixelWidth;
+		pixelPosition.y /= cam.pixelHeight;
+		return pixelPosition;
+	}
+
+
+	// Focus position
+	public static Vector2 GetFocusPosition()
+	{
+		Vector3 focus;
+		if(useMouse)
+			 focus = Input.mousePosition;
+		else
+			throw new NotImplementedException("Gaze focus point functionality not implemented yet!");
+		
+		return new Vector2(focus.x, focus.y);
+	}
+
+	public static Vector2 GetNormalizedFocusPosition()
+	{
+		Vector2 pixelFocus = GetFocusPosition();
+		return NormalizeScreenPosition(pixelFocus);
+	}
+}
---
+++ BootCamp/Assets/Custom/ScreenFade.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ScreenFade : MonoBehaviour {
+
+	public Shader fadeShader = null;	
+	public Color fadeColour = new Color(0,0,0,1);
+
+	private int blinkFramesToGo = 0;
+	private int fadeOutFramesToGo = 0;
+	private int fadeOutFramesTotal = 1;
+	private int fadeInFramesToGo = 0;
+	private int fadeInFramesTotal = 1;
+
+	static Material m_Material = null;
+	protected Material material {
+		get {
+			if (m_Material == null) {
+				m_Material = new Material(fadeShader);
+				m_Material.hideFlags = HideFlags.DontSave;
+			}
+			return m_Material;
+		} 
+	}
+
+	void Update()
+	{
+		// TODO move this functionality AWAY from this class!
+		if(Input.GetKey("o"))
+		{
+			Blink(1);
+		}	
+		else if(Input.GetKey("i"))
+		{
+			Blink(6);
+		}
+		else if(Input.GetKey("p"))
+		{
+			FadeOutIn(24,24);
+		}	
+	}
+
+	public void Blink(int noOfFrames)
+	{
+		blinkFramesToGo = noOfFrames;
+	}
+
+	public void FadeIn(int noOfFrames)
+	{
+		fadeInFramesToGo = noOfFrames;
+		fadeInFramesTotal = noOfFrames;
+	}
+
+	public void FadeOut(int noOfFrames)
+	{
+		fadeOutFramesToGo = noOfFrames;
+		fadeOutFramesTotal = noOfFrames;
+	}
+
+	public void FadeOutIn(int noOfFramesOut, int noOfFramesIn)
+	{
+		FadeOut(noOfFramesOut);
+		FadeIn(noOfFramesIn);
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture dest)
+	{	
+		float fadeAmount = 0;
+		if(blinkFramesToGo-- > 0)
+		{
+			fadeAmount = 1;
+		}
+		else if(fadeOutFramesToGo-- > 0)
+		{
+			fadeAmount = 1.0f - (float)(fadeOutFramesToGo) / fadeOutFramesTotal;
+		}
+		else if(fadeInFramesToGo-- > 0)
+		{
+			fadeAmount = (float)(fadeInFramesToGo) / fadeInFramesTotal;
+		}
+
+		material.SetColor("_Colour", fadeColour);
+		material.SetFloat("_FadeAmount", fadeAmount);
+
+		Graphics.Blit(source, dest, material);
+	}
+}
